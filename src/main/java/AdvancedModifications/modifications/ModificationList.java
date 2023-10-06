36bef16c169ea6c5e0e3e4ab647f900243551a93
Jonathan Cheng <jcheng@sewickley.org>
2023-10-06 10:24:29 -0400

    Added method to get a mod from string

--- src/main/java/AdvancedModifications/modifications/ModificationList.java
+++ src/main/java/AdvancedModifications/modifications/ModificationList.java
@@ -1,5 +1,7 @@
 package advancedmodifications.modifications;
 
+import net.md_5.bungee.api.ChatColor;
+
 public enum ModificationList {
     ;
 
@@ -10,5 +12,42 @@ public enum ModificationList {
 
     public Modification getReference() {
         return reference;
+    }
+
+    /**
+     * 
+     * @param str String to check
+     * @param exactness (0-3) Level of exactness
+     * @return Mod if found, null if not
+     */
+    public Modification modFromString(String str, int exactness) {
+        if(exactness < 0 || exactness > 3)
+            throw new IllegalArgumentException("Exactness must be between 0 and 3");
+
+        for(ModificationList mod : ModificationList.values()) {
+            switch(exactness) {
+                case 0:
+                    if(mod.getReference().getName().startsWith(str))
+                        return mod.getReference();
+                    break;
+
+                case 1:
+                    if(ChatColor.stripColor(mod.getReference().getName()).equalsIgnoreCase(str))
+                        return mod.getReference();
+                    break;
+
+                case 2:
+                    if(mod.getReference().getName().equalsIgnoreCase(str))
+                        return mod.getReference();
+                    break;
+
+                case 3:
+                    if(mod.getReference().getName().equals(str))
+                        return mod.getReference();
+                    break;
+            }
+        }
+        
+        return null;
     }
 }
