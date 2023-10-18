8866294f8eec404c0514e84415468eb549c3cf49
Multiplyster <boylecheng@gmail.com>
2023-10-18 14:01:49 -0400

    Added inheritable Modular Item class

--- src/main/java/AdvancedModifications/items/modularitems/ModularItem.java
+++ src/main/java/AdvancedModifications/items/modularitems/ModularItem.java
@@ -16,31 +16,45 @@ public abstract class ModularItem extends CustomItem {
 
     public static ChatColor[] nameColors = new ChatColor[] {ChatColor.GREEN, ChatColor.GOLD, ChatColor.DARK_PURPLE};
 
-    public ModularItem(Material material, String name, String[] lore) {
-        super(material, name, lore);
+    public ModularItem(Material material, String name) {
+        super(material, name, new String[] {});
     }
 
+    /**
+     * Upgrades the given item
+     * @param item Item to upgrade
+     */
     public static void upgrade(ItemStack item) {
         if(!item.getType().toString().contains("netherite"))
             return;
 
         ItemMeta meta = item.getItemMeta();
         
-        if(!meta.hasLore()) { /* No lore means base netherite item */
+        /* No lore means base netherite item */
+        if(!meta.hasLore()) {
             List<String> lore = new ArrayList<String>();
             lore.add(nameColors[0] + "Modular" + (new ItemStack(item.getType()).getItemMeta().getDisplayName() + "MKI"));
+            lore.add(ChatColor.GRAY + "Available Modification Slots: 1/3");
+            lore.add(ChatColor.GRAY + "Slot 1: " + ChatColor.ITALIC + "EMPTY");
+            lore.add(ChatColor.GRAY + "Slot 2: " + ChatColor.ITALIC + "" + ChatColor.RED + "LOCKED");
+            lore.add(ChatColor.GRAY + "Slot 3: " + ChatColor.ITALIC + "" + ChatColor.RED + "LOCKED");
             meta.setLore(lore);
             return;
         }
         
+        /* Assume the item is a modular item */
         ModularItem i = (ModularItem) item;
         List<String> lore = meta.getLore();
         int tier = i.getTier();
 
+        /* Replace first line of old lore with new string */
         lore.set(0, nameColors[tier] + " Modular" + (new ItemStack(item.getType()).getItemMeta().getDisplayName() + "MK" + romanNumerFromInt(tier + 1)));
         meta.setLore(lore);
     }
 
+    /**
+     * @return The tier of this item based on the first line of the lore
+     */
     public int getTier() {
         List<String> lore = this.getItemMeta().getLore();
         if(lore.isEmpty())
