331cfc76cf41b84098cf2e968172587310486ff2
Multiplyster <boylecheng@gmail.com>
2023-10-18 08:00:26 -0400

    Added modular item base

---
+++ src/main/java/AdvancedModifications/items/modularitems/ModularItem.java
@@ -0,0 +1,86 @@
+package AdvancedModifications.items.modularitems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import AdvancedModifications.items.util.CustomItem;
+
+public abstract class ModularItem extends CustomItem {
+
+    public static ChatColor[] nameColors = new ChatColor[] {ChatColor.GREEN, ChatColor.GOLD, ChatColor.DARK_PURPLE};
+
+    public ModularItem(Material material, String name, String[] lore) {
+        super(material, name, lore);
+    }
+
+    public static void upgrade(ItemStack item) {
+        if(!item.getType().toString().contains("netherite"))
+            return;
+
+        ItemMeta meta = item.getItemMeta();
+        
+        if(!meta.hasLore()) { /* No lore means base netherite item */
+            List<String> lore = new ArrayList<String>();
+            lore.add(nameColors[0] + "Modular" + (new ItemStack(item.getType()).getItemMeta().getDisplayName() + "MKI"));
+            meta.setLore(lore);
+            return;
+        }
+        
+        ModularItem i = (ModularItem) item;
+        List<String> lore = meta.getLore();
+        int tier = i.getTier();
+
+        lore.set(0, nameColors[tier] + " Modular" + (new ItemStack(item.getType()).getItemMeta().getDisplayName() + "MK" + romanNumerFromInt(tier + 1)));
+        meta.setLore(lore);
+    }
+
+    public int getTier() {
+        List<String> lore = this.getItemMeta().getLore();
+        if(lore.isEmpty())
+            return 0;
+
+        String firstLine = lore.get(0);
+        if(!firstLine.contains("MK"))
+            return 0;
+
+        return numFromRomanNumeral(firstLine.substring(firstLine.indexOf("MK")));
+    }
+
+    /** Only goes up to 3 */
+    public static int numFromRomanNumeral(String num) {
+        num = num.toLowerCase();
+
+        switch(num) {
+            case "i": return 1;
+            case "ii": return 2;
+            case "iii": return 3;
+            default: return 0;
+        }
+    }
+
+    /** Only goes up to 3 */
+    public static String romanNumerFromInt(int num) {
+        switch(num) {
+            case 1: return "I";
+            case 2: return "II";
+            case 3: return "III";
+            default: return null;
+        }
+    }
+
+    @Override
+    public abstract ShapedRecipe getRecipe();
+
+    @Override
+    public void onEvent(Event e) {
+        throw new UnsupportedOperationException("Unimplemented method 'onEvent'");
+    }
+    
+}
